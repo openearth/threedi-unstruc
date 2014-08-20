4b46d2e31c6e990685999e8647a1084353e73bd4
Fedor Baart <fedor.baart@deltares.nl>
2014-08-20 20:59:02 +0200

    interaction

--- app/scripts/renders.js
+++ app/scripts/renders.js
@@ -17,6 +17,7 @@ render.dflowfm = function(model){
         // Use overlays as a context
         console.log(data);
         model.grid = data;
+        model.variable = 's1';
         var svg = d3.select(map.getPanes().overlayPane).append('svg');
         svg
             .attr('id', 'grid' + model.uuid);
@@ -52,7 +53,17 @@ render.dflowfm = function(model){
                 .enter()
                 .append('path')
                 .attr('id', function(feature) {return 'c' + feature.properties.index;})
-                .attr('d', path);
+                .attr('d', path)
+                .on('click', function(feature){
+                    model.ws.send(JSON.stringify({
+                        'set_var_index': model.variable, 
+                        'index': [feature.properties.index],
+                        'dtype': 'double', 
+                        'shape': [1]
+                    }));
+                    model.ws.send(new Float64Array([10]));
+                });
+        
 
         map.on('viewreset', reset);
         reset();
@@ -61,34 +72,23 @@ render.dflowfm = function(model){
     
 };
 
+
 update.dflowfm = function(model) {
-    model.ws.send(JSON.stringify({'get_var': 's1'}));
+    var name = model.variable;
     var color = d3.scale.linear()
             .domain([-1, 0, 1])
             .range(['navy', '#20B2AA', 'turquoise']);
-            // .range(['#1C6BA0', '#78B6A4', '#E4F5E9']);
-
-    var opacity = d3.scale.linear()
-            .domain([-0.5, 0, 0.5])
-            .range([0.8 ,0.2, 0.5]);
 
     d3.selectAll('path')
         .style('fill', function(feature){ 
-            var s1 = 0;
-            if ('s1' in model.vars) {
-                s1 = model.vars.s1[feature.properties.index];
+            var value = 0;
+            if (name in model.vars) {
+                value = model.vars[name][feature.properties.index];
             } 
-            return color(s1);
-        })
-        .style('fill-opacity', function(feature){ 
-            var s1 = 0;
-            if ('s1' in model.vars) {
-                s1 = model.vars.s1[feature.properties.index];
-            } 
-            return opacity(s1);
+            return color(value);
         });
-
 };
+
 
 play.dflowfm = function(model){
     console.log('setting model update', model.update );
